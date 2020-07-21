fd5057a429c6d20b99ba49774021597dd5c6478b
Eric Abouaf <eric.abouaf@gmail.com>
2020-07-21 11:21:13 +0200

    Add iconv conversion of sendgrid charsets

--- Controller/SendgridController.php
+++ Controller/SendgridController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SendgridController extends AbstractWebhookController
 {
+    const UTF8 = 'utf8';
+
     /**
      * @param Request $request
      * @return JsonResponse
@@ -34,6 +36,33 @@ class SendgridController extends AbstractWebhookController
         if (array_key_exists('html', $qp)) {
             $html = $qp['html'];
         }
+
+        // charsets
+        $charsets = json_decode($qp['charsets'], true);
+        if ($charsets) {
+
+            if ($charsets['to'] != self::UTF8) {
+                $to = iconv($charsets['to'] , self::UTF8, $to);
+            }
+
+            if (!is_null($html) && $charsets['html'] != self::UTF8) {
+                $html = iconv($charsets['html'] , self::UTF8, $html);
+            }
+
+            if ($charsets['subject'] != self::UTF8) {
+                $subject = iconv($charsets['subject'] , self::UTF8, $subject);
+            }
+
+            if ($charsets['from'] != self::UTF8) {
+                $from = iconv($charsets['from'] , self::UTF8, $from);
+            }
+
+            if (!is_null($text) && $charsets['text'] != self::UTF8) {
+                $text = iconv($charsets['text'] , self::UTF8, $text);
+            }
+
+        }
+
 
         $event = new InboundEmailEvent($from, $to, $subject, $text, $html);
         $this->dispatch($event);
